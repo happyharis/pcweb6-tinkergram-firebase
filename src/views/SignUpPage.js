a1845d239ce07687bddd19c202edfd3d1db7a448
Haris Samingan <muhamad_haris@hotmail.com>
2022-12-19 06:22:16 +0800

    Add sign up page

--- src/views/SignUpPage.js
+++ src/views/SignUpPage.js
@@ -1,10 +1,14 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
 
 export default function SignUpPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
   return (
     <Container>
       <h1 className="my-3">Sign up for an account</h1>
@@ -32,9 +36,24 @@ export default function SignUpPage() {
           />
           <a href="/login">Have an existing account? Login here.</a>
         </Form.Group>
-        <Button variant="primary">Sign Up</Button>
+        <Button
+          variant="primary"
+          onClick={async (e) => {
+            setError("");
+            const canSignup = username && password;
+            if (canSignup)
+              try {
+                await createUserWithEmailAndPassword(auth, username, password);
+                navigate("/");
+              } catch (error) {
+                setError(error.message);
+              }
+          }}
+        >
+          Sign Up
+        </Button>
       </Form>
-      <p></p>
+      <p>{error}</p>
     </Container>
   );
 }
